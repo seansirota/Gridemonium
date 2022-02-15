be58294fcb55439a9bffcb254aaf8daf9ece2404
SEAN.SIROTA <SEAN.SIROTA@baruchmail.cuny.edu>
2022-02-15 11:46:45 -0500

    More comments.

--- Bubble.cs
+++ Bubble.cs
@@ -18,6 +18,7 @@ namespace Gridemonium
         public char Letter { get; set; }
         public int Number { get; set; }
 
+        //Constructor when all Bubbles in grid are initialized. No new Bubble objects can be created or destroyed, only changed.
         public Bubble(char letter, int number, PictureBox box)
         {
             VisualComponent = box;
@@ -29,6 +30,7 @@ namespace Gridemonium
             State = BubbleState.Null;
         }
 
+        //Basic method for "spawning" a bubble. It actually checks if the picture box of the bubble object is null or not and then changes the image to something else.
         public int SpawnBubble(Dictionary<string, Bubble> BubbleGrid, char col, bool waitFlag)
         {
             Bubble bubble = BubbleGrid["Bubble" + col.ToString() + "1"];
@@ -44,6 +46,7 @@ namespace Gridemonium
             return bubble.Number;
         }
 
+        //Basic method for implementing a dropping mechanism when there is space beneath a bubble to "fall" to.
         public int BubbleFall(Dictionary<string, Bubble> BubbleGrid, char col, int row, bool waitFlag)
         {
             Bubble bubbleUp = BubbleGrid["Bubble" + col.ToString() + row.ToString()];
@@ -65,6 +68,7 @@ namespace Gridemonium
             return bubbleDown.Number;
         }
 
+        //Updates the image of the bubble object as well as the picture box it is tied to in one method.
         public void ImageUpdate(Bubble bubble, Image image)
         {
             bubble.Image = image;
